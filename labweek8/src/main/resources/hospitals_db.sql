ffe2d131c4e01836db5eac437955477201f83910
pugeaale <pugeaale@gmail.com>
2024-10-08 10:48:45 +0200

    :sparkles: implementing create tables for patients and employees

--- labweek8/src/main/resources/hospitals_db.sql
+++ labweek8/src/main/resources/hospitals_db.sql
@@ -1,3 +1,18 @@
 CREATE DATABASE hospitals_db;
 
 USE hospitals_db;
+
+CREATE TABLE employees (
+						  employee_id INT PRIMARY KEY,
+						  department VARCHAR(50),
+						  name VARCHAR(100),
+						  status VARCHAR(20)
+);
+
+CREATE TABLE patients (
+						 patient_id INT PRIMARY KEY,
+						 name VARCHAR(100),
+						 date_of_birth DATE,
+						 admitted_by INT,
+						 FOREIGN KEY (admitted_by) REFERENCES employees(employee_id)
+);
