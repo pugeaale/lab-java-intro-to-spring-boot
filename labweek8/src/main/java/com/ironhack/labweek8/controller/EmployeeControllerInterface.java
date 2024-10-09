95405b764b95c3a419234c8f0a758d1b420936dc
pugeaale <pugeaale@gmail.com>
2024-10-09 08:09:51 +0200

    :sparkles: implemented employee controller interface

---
+++ labweek8/src/main/java/com/ironhack/labweek8/controller/EmployeeControllerInterface.java
@@ -0,0 +1,10 @@
+package com.ironhack.labweek8.controller;
+
+import com.ironhack.labweek8.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeControllerInterface {
+
+    List<Employee> getEmployees();
+}
